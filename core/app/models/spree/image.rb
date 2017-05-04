1bb462e1ec7ce9c60ade6a5f31eb1f5c25b77bd2
Farrah Brink <febrink@gmail.com>
2017-05-03 23:36:43 -0400

    Fix image blurring.

--- core/app/models/spree/image.rb
+++ core/app/models/spree/image.rb
@@ -3,11 +3,11 @@ module Spree
     validate :no_attachment_errors
 
     has_attached_file :attachment,
-                      styles: { mini: '48x48>', small: '100x100>', product: '240x240>', large: '600x600>' },
-                      default_style: :product,
-                      url: '/spree/products/:id/:style/:basename.:extension',
-                      path: ':rails_root/public/spree/products/:id/:style/:basename.:extension',
-                      convert_options: { all: '-strip -auto-orient -colorspace sRGB -quality 100' }
+      styles: { mini: '48x48>', small: '100x100>', product: '240x240>', large: '600x600>' },
+      default_style: :product,
+      url: '/spree/products/:id/:style/:basename.:extension',
+      path: ':rails_root/public/spree/products/:id/:style/:basename.:extension',
+      convert_options: { all: '-strip -auto-orient -colorspace sRGB -quality 100' }
     validates_attachment :attachment,
       presence: true,
       content_type: { content_type: %w(image/jpeg image/jpg image/png image/gif) }
