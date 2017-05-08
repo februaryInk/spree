3f0117a1dda3a85cf48ef93bc4e4aa41b828965a
Farrah Brink <febrink@gmail.com>
2017-05-08 11:59:02 -0400

    To preserve customizations, do not combine line items.

--- core/app/models/spree/order_contents.rb
+++ core/app/models/spree/order_contents.rb
@@ -75,20 +75,16 @@ module Spree
     end
 
     def add_to_line_item(variant, quantity, options = {})
-      line_item = grab_line_item_by_variant(variant, false, options)
+      opts = ActionController::Parameters.new(options.to_h).
+        permit(PermittedAttributes.line_item_attributes).to_h.
+        merge( { currency: order.currency } )
 
-      if line_item
-        line_item.quantity += quantity.to_i
-        line_item.currency = currency unless currency.nil?
-      else
-        opts = ActionController::Parameters.new(options.to_h).
-               permit(PermittedAttributes.line_item_attributes).to_h.
-               merge( { currency: order.currency } )
-
-        line_item = order.line_items.new(quantity: quantity,
-                                          variant: variant,
-                                          options: opts)
-      end
+      line_item = order.line_items.new(
+        quantity: quantity,
+        variant: variant,
+        options: opts
+      )
+      
       line_item.target_shipment = options[:shipment] if options.has_key? :shipment
       line_item.save!
       line_item
