08375cb373b74e92909d27f36746dfafd1ccd445
Farrah Brink <febrink@gmail.com>
2017-10-18 09:34:13 -0400

    Fix delete button helper.

--- backend/app/helpers/spree/admin/navigation_helper.rb
+++ backend/app/helpers/spree/admin/navigation_helper.rb
@@ -140,7 +140,7 @@ module Spree
         url = options[:url] || object_url(resource)
         name = options[:name] || Spree.t(:delete)
         options[:class] = "btn btn-danger btn-sm delete-resource"
-        options[:data] = { confirm: Spree.t(:are_you_sure), action: 'remove' }
+        options[:data] = { confirm: Spree.t(:are_you_sure), action: 'remove', method: :delete }
         link_to_with_icon 'delete', name, url, options
       end
 
