fb2808a9529a867c476c7233c29646494aa6ed78
Farrah Brink <febrink@gmail.com>
2017-11-03 11:54:14 -0400

    Add a preview image to fonts.

--- core/app/models/spree/font.rb
+++ core/app/models/spree/font.rb
@@ -6,8 +6,10 @@ module Spree
     has_many :font_line_items, dependent: :destroy, class_name: 'Spree::FontLineItem'
     has_many :line_items, through: :font_line_items, class_name: 'Spree::LineItem'
 
-    has_one :image, as: :viewable, dependent: :destroy, class_name: 'Spree::Image'
+    has_one :image, -> { where( { position: 2 } ) }, as: :viewable, dependent: :destroy, class_name: 'Spree::Image'
+    has_one :preview_image, -> { where( { position: 1 } ) }, as: :viewable, dependent: :destroy, class_name: 'Spree::Image'
 
     accepts_nested_attributes_for :image
+    accepts_nested_attributes_for :preview_image
   end
 end
